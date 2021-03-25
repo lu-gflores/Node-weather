842c115994477b0e96ba5171d831a113becd9aa6
George Flores <georgeflores886@gmail.com>
2021-03-25 18:10:35 -0400

    updated index and styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,26 @@
 const weatherForm = document.querySelector('form')
 const userInput = document.querySelector('input')
+const message1 = document.getElementById('message-1')
+const message2 = document.getElementById('message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = userInput.value
+
+    message1.textContent = ''
+    message2.textContent = ''
+
     fetch(`http://localhost:3000/weather?address=${location}`)
         .then(res => {
             res.json().then(data => {
                 if (data.error) {
-                    console.log(data.error)
+                    message1.textContent = data.error
                 } else {
-                    console.log(data.location)
-                    console.log(data.forecast)
+                    message1.textContent = data.location
+                    message2.textContent = data.forecast
                 }
             })
         })
-
 })
 
 
