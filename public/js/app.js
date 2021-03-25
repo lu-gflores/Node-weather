4c1097f4d7300181a484d5e6755fc711b1005566
George Flores <georgeflores886@gmail.com>
2021-03-25 16:28:44 -0400

    created fetch method from api

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-console.log('Client side js file is loaded')
+fetch('http://localhost:3000/weather?address=boston')
+    .then(res => {
+        res.json().then(data => {
+            if (data.err) {
+                console.log(data.err)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
